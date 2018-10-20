0eb6de489fc39d0c084bc25379d916b215e6cfc0
kaurnavjeet <kaurnavjeet09@gmail.com>
2018-10-20 11:01:08 -0700

    Assignment finished

---
+++ main.js
@@ -0,0 +1,14 @@
+const greetings = require("./greeting");
+const goodbye = require("./goodbye");
+
+greetings.greetingEnglish();
+goodbye.goodbyeEnglish();
+
+greetings.greetingJapanese();
+goodbye.goodbyeJapanese();
+
+greetings.greetingSpanish();
+goodbye.goodbyeSpanish();
+
+greetings.greetingIcelandic();
+goodbye.goodbyeIcelandic();
